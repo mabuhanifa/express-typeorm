217a45d8cd64616de31f21709641e4fb6a52f2a8
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-19 15:02:00 +0600

    express app working

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.json());
+console.log(`object`);
+
+app.post("/", (req: Request, res: Response) => {
+  const data = req.body;
+  try {
+    console.log(data);
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
